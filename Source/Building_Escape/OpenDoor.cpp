3f620d1ee61dbf2beb7a280243952a02ee2476a0
DannyPadron09 <danny.padron09@gmail.com>
2021-06-04 11:27:04 -0400

    door opens when game starts

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -31,11 +31,16 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	UE_LOG(LogTemp, Warning, TEXT("%s"), *GetOwner()->GetActorRotation().ToString());
 	UE_LOG(LogTemp, Warning, TEXT("Yaw is : %f"), GetOwner()->GetActorRotation().Yaw);
 
-	
+	float CurrentYaw = GetOwner()->GetActorRotation().Yaw;
+
+	FRotator OpenDoor(0.f, TargetYaw, 0.f);
+	OpenDoor.Yaw = FMath::Lerp(CurrentYaw, TargetYaw, 0.02f);	
+	GetOwner()->SetActorRotation(OpenDoor);
+
+
 	// FRotator CurrentRotation = GetOwner()->GetActorRotation();
 	// CurrentRotation.Yaw = 90.f;
 	// FRotator OpenDoor(0.f, 90.f, 0.f);
-	// GetOwner()->SetActorRotation(OpenDoor);
 
 }
 
