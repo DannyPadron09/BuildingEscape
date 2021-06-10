591fa8e82f6effa77b39facf5c79a2cd3d49c77a
DannyPadron09 <danny.padron09@gmail.com>
2021-06-10 11:12:15 -0400

    added the trigger volume to the door

--- Source/Building_Escape/OpenDoor.h
+++ Source/Building_Escape/OpenDoor.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
 
@@ -33,4 +34,8 @@ private:
 	UPROPERTY(EditAnywhere)
 	float TargetYaw = -90.f;
 
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume* PressurePlate;
+	
 };
