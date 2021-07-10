4eb5bbc4882e0a042ba15a39c4845d630c1fc07d
DannyPadron09 <danny.padron09@gmail.com>
2021-07-10 11:07:46 -0400

    more cleaning

--- Source/Building_Escape/Grabber.h
+++ Source/Building_Escape/Grabber.h
@@ -17,14 +17,13 @@ public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
 private:
 	float Reach = 100.f;
 
